43a0112df82a02c8365fdea03b1a049f630f217c
OUT Rodriguez <outmjrv@banrural.com.gt>
2026-02-05 11:47:24 -0600

    Integracion de consumo COBIS externo

---
+++ Resilience4J/Mario/Omnicanalidad4J/src/main/java/com/Omnicanalidad/Omnicanalidad4J/Data/DatosRequestPrelogin.java
@@ -0,0 +1,26 @@
+package com.Omnicanalidad.Omnicanalidad4J.Data;
+
+public class DatosRequestPrelogin {
+
+    private String password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    private String login;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
+
